7c5a113268fdb9d9bc66538945b7a06d39746fee
Brian Dewey <bdewey@gmail.com>
2018-12-16 08:59:39 -0800

    +comments, -dead code

--- CommonplaceBookApp/PageProperties.swift
+++ CommonplaceBookApp/PageProperties.swift
@@ -10,11 +10,24 @@ import enum TextBundleKit.Result
 
 /// Metadata about pages in a Notebook.
 public struct PageProperties: Codable {
+
+  /// FileMetadata identifying this page in a FileMetadatProvider.
   public var fileMetadata: FileMetadata
+
+  /// Hashtags present in the page.
   public let hashtags: [String]
+
+  /// Title of the page.
   public let title: String
+
+  /// All card templates in the page.
   public let cardTemplates: [CardTemplateSerializationWrapper]
 
+  /// Designated initializer.
+  ///
+  /// - parameter fileMetadata: FileMetadata identfying this page in a FileMetadataProvider.
+  /// - parameter nodes: Parsed MiniMarkdown nodes from the content of the page.
+  ///                    Relvant page properties are extracted from these nodes.
   public init(fileMetadata: FileMetadata, nodes: [Node]) {
     self.fileMetadata = fileMetadata
     self.hashtags = nodes.hashtags
@@ -22,12 +35,20 @@ public struct PageProperties: Codable {
     self.cardTemplates = nodes.cardTemplates
   }
 
+  /// Returns a copy of the PageProperties with the file metadata changed.
   public func updatingFileMetadata(_ fileMetadata: FileMetadata) -> PageProperties {
     var copy = self
     copy.fileMetadata = fileMetadata
     return copy
   }
 
+  /// Loads PageProperties from an item in a FileMetadataProvider.
+  ///
+  /// - parameter metadataWrapper: The FileMetadata identifying the page.
+  /// - parameter metadataProvider: The container of the page.
+  /// - parameter parsingRules: Rules for parsing the contents of the page into nodes.
+  /// - parameter completion: Routine called with the resulting properties.
+  ///                         Called on the main thread.
   public static func loadProperties(
     from metadataWrapper: FileMetadata,
     in metadataProvider: FileMetadataProvider,
@@ -63,19 +84,32 @@ public struct PageProperties: Codable {
 }
 
 extension PageProperties {
+
+  /// Kinds of errors when loading properties.
   enum Error: Swift.Error {
+
+    /// Cannot get an EditableDocument for a specific FileMetadata.
     case noEditableDocument
+
+    /// Cannot open an EditableDocument.
     case cannotOpenDocument
   }
 }
 
 extension PageProperties: CustomStringConvertible {
+
+  /// For debugging: A string representation of the properties.
+  /// TODO: Consider making this a mirror instead.
   public var description: String {
     return "\(title) \(fileMetadata)"
   }
 }
 
 extension Array where Element == Node {
+
+  /// For an array of Nodes, return all VocabularyAssociations and ClozeTemplates found in
+  /// the nodes.
+  /// TODO: Make this extensible for other card template types.
   var cardTemplates: [CardTemplateSerializationWrapper] {
     var results = [CardTemplateSerializationWrapper]()
     results.append(
@@ -87,19 +121,46 @@ extension Array where Element == Node {
     )
     return results
   }
+
+  /// Extracts the title from an array of nodes.
+  ///
+  /// - note: If there is a heading anywhere in the nodes, the contents of the first heading
+  ///         is the title. Otherwise, the contents of the first non-blank line is the title.
+  var title: String {
+    if let heading = self.lazy.compactMap({ $0.first(where: { $0.type == .heading }) }).first {
+      return MarkdownStringRenderer.textOnly.render(node: heading)
+    } else if let notBlank = self.lazy.compactMap({
+      $0.first(where: { $0.type != .blank })
+    }).first {
+      return MarkdownStringRenderer.textOnly.render(node: notBlank)
+    } else {
+      return ""
+    }
+  }
+
+  /// Extracts all hashtags from nodes.
+  var hashtags: [String] {
+    let hashtagSet = self
+      .map { $0.findNodes(where: { $0.type == .hashtag }) }
+      .joined()
+      .reduce(into: Set<String>()) { $0.insert(String($1.slice.substring)) }
+    return [String](hashtagSet)
+  }
 }
 
+/// Wrapper around PageProperties for IGListKit.
 public final class PagePropertiesListDiffable: ListDiffable {
+
+  /// The wrapped PageProperties.
   public private(set) var value: PageProperties
+
+  /// How many cards are eligible for study in this page.
   public private(set) var cardCount: Int
 
+  /// Designated initializer.
   public init(_ value: PageProperties, cardCount: Int) {
     self.value = value
     self.cardCount = cardCount
-  }
-
-  public func updateMetadata(_ metadata: FileMetadata) {
-    value.fileMetadata = metadata
   }
 
   public func diffIdentifier() -> NSObjectProtocol {
@@ -115,32 +176,11 @@ public final class PagePropertiesListDiffable: ListDiffable {
   }
 }
 
+/// Debuggability extensions for PagePropertiesListDiffable.
 extension PagePropertiesListDiffable: CustomStringConvertible, CustomDebugStringConvertible {
   public var description: String { return value.description }
   public var debugDescription: String {
-    return "DocumentPropertiesListDiffable \(Unmanaged.passUnretained(self).toOpaque()) " + value.description
-  }
-}
-
-/// Helpers for extracting document properties from nodes.
-extension Array where Element == Node {
-  var title: String {
-    if let heading = self.lazy.compactMap({ $0.first(where: { $0.type == .heading }) }).first {
-      return MarkdownStringRenderer.textOnly.render(node: heading)
-    } else if let notBlank = self.lazy.compactMap({
-      $0.first(where: { $0.type != .blank })
-    }).first {
-      return MarkdownStringRenderer.textOnly.render(node: notBlank)
-    } else {
-      return ""
-    }
-  }
-
-  var hashtags: [String] {
-    let hashtagSet = self
-      .map { $0.findNodes(where: { $0.type == .hashtag }) }
-      .joined()
-      .reduce(into: Set<String>()) { $0.insert(String($1.slice.substring)) }
-    return [String](hashtagSet)
+    return "DocumentPropertiesListDiffable \(Unmanaged.passUnretained(self).toOpaque()) "
+      + value.description
   }
 }
