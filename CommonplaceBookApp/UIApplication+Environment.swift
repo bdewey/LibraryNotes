62232b062a6f62d466523e2e696d68e1ecf083fd
Brian Dewey <bdewey@gmail.com>
2019-08-20 11:42:21 -0700

    Swiftformat

--- CommonplaceBookApp/UIApplication+Environment.swift
+++ CommonplaceBookApp/UIApplication+Environment.swift
@@ -1,3 +1,5 @@
+// Copyright © 2017-present Brian's Brain. All rights reserved.
+
 import UIKit
 
 @nonobjc
@@ -5,9 +7,9 @@ extension UIApplication {
   /// True if the app runs in the simulator.
   static var isSimulator: Bool {
     #if targetEnvironment(simulator)
-    return true
+      return true
     #else
-    return false
+      return false
     #endif
   }
 }
