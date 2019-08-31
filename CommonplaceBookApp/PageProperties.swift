617baf28a4a0b054d9d013a804f59239eaf2375a
Brian Dewey <bdewey@gmail.com>
2019-08-31 08:16:28 -0700

    Add capability to store and manipulate "naked" properties (#10)

--- CommonplaceBookApp/PageProperties.swift
+++ CommonplaceBookApp/PageProperties.swift
@@ -6,43 +6,43 @@ import Yams
 /// Metadata about pages in a NoteBundle.
 public struct PageProperties: Codable, Hashable {
   /// SHA-1 digest of the contents of the page.
-  public let sha1Digest: String
+  public var sha1Digest: String?
 
   /// Last modified time of the page.
-  public let timestamp: Date
+  public var timestamp: Date
 
   /// Hashtags present in the page.
-  public let hashtags: [String]
+  /// - note: Need to keep sorted to make comparisons canonical. Can't be a Set or serialization isn't canonical :-(
+  public var hashtags: [String]
 
   /// Title of the page. May include Markdown formatting.
-  public let title: String
+  public var title: String
 
   /// IDs of all card templates in the page.
-  public let cardTemplates: [String]
+  /// - note: Need to keep sorted to make comparisons canonical. Can't be a Set or serialization isn't canonical :-(
+  public var cardTemplates: [String]
 
-  init(
-    sha1Digest: String,
-    timestamp: Date,
-    hashtags: [String],
-    title: String,
-    cardTemplates: [String]
+  public func makeSnippet() throws -> TextSnippet {
+    let text = try YAMLEncoder().encode(self)
+    return TextSnippet(text)
+  }
+
+  public init(
+    sha1Digest: String? = nil,
+    timestamp: Date = Date(),
+    hashtags: [String] = [],
+    title: String = "",
+    cardTemplates: [String] = []
   ) {
     self.sha1Digest = sha1Digest
     self.timestamp = timestamp
-    // Sorting the arrays to make equality comparisons canonical
-    // Consider turning the arrays into sets?
     self.hashtags = hashtags.sorted()
     self.title = title
     self.cardTemplates = cardTemplates.sorted()
   }
 
-  func makeSnippet() throws -> TextSnippet {
-    let text = try YAMLEncoder().encode(self)
-    return TextSnippet(text)
-  }
-
   /// Construct PageProperties from encoded YAML.
-  init(_ snippet: TextSnippet) throws {
+  public init(_ snippet: TextSnippet) throws {
     self = try YAMLDecoder().decode(PageProperties.self, from: snippet.text)
   }
 }
