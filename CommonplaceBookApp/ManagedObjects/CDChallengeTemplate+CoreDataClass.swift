732a34935cbb82bae9ef32311574581973fe7179
Brian Dewey <bdewey@gmail.com>
2019-10-02 19:33:10 -0700

    whoa you don't need to check in codegen files!

--- CommonplaceBookApp/ManagedObjects/CDChallengeTemplate+CoreDataClass.swift
+++
@@ -1,7 +0,0 @@
-// Copyright © 2017-present Brian's Brain. All rights reserved.
-
-import CoreData
-import Foundation
-
-@objc(CDChallengeTemplate)
-public class CDChallengeTemplate: NSManagedObject {}
