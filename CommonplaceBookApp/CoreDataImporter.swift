450f2bd499cbdfcd2118caaf9555017ab43de75d
Brian Dewey <bdewey@gmail.com>
2019-10-03 19:55:19 -0700

    Bit of code cleanup

--- CommonplaceBookApp/CoreDataImporter.swift
+++ CommonplaceBookApp/CoreDataImporter.swift
@@ -16,53 +16,15 @@ public enum CoreDataImporter {
     persistentContainer.loadPersistentStores { (description, error) in
       if let error = error {
         logger.error("Error opening persistent store: \(error)")
+        return
       } else {
         logger.info("Opened persistent store at \(description.url?.absoluteString ?? "")")
       }
       let backgroundContext = persistentContainer.newBackgroundContext()
       backgroundContext.perform {
+        importAllPages(from: notebook, into: backgroundContext)
         for (key, properties) in notebook.pageProperties {
-          let uuid = UUID(uuidString: key)!
-          let page = CDPage.getOrCreate(uuid: uuid, context: backgroundContext)
-          page.timestamp = properties.timestamp
-          page.title = properties.title
-          for hashtag in properties.hashtags {
-            let hashtagRecord = CDHashtag.getOrCreate(name: hashtag, context: backgroundContext)
-            page.hashtags = page.hashtags?.adding(hashtagRecord) as NSSet?
-          }
-          if let contents = try? notebook.currentTextContents(for: key) {
-            page.contents.flatMap { backgroundContext.delete($0) }
-            let contentsObject = CDPageContents(context: backgroundContext)
-            contentsObject.contents = contents
-            page.contents = contentsObject
-          }
-          // Delete all existing templates
-          backgroundContext.deleteAllObjects(in: page.challengeTemplates)
-          // Import templates
-          for templateKeyString in properties.cardTemplates {
-            guard
-              let templateKey = ChallengeTemplateArchiveKey(templateKeyString),
-              let template = notebook.challengeTemplate(for: templateKeyString)
-            else {
-              continue
-            }
-            do {
-              let templateObject = CDChallengeTemplate(context: backgroundContext)
-              templateObject.serialized = try YAMLEncoder().encode(template)
-              templateObject.type = templateKey.type
-              // Import challenges
-              let challenges = template.challenges.map { challenge -> CDChallenge in
-                let challengeObject = CDChallenge(context: backgroundContext)
-                challengeObject.key = String(describing: challenge.challengeIdentifier.index)
-                return challengeObject
-              }
-              templateObject.challenges = Set(challenges) as NSSet
-              page.challengeTemplates = page.challengeTemplates?.adding(templateObject) as NSSet?
-              logger.info("Imported template \(templateKeyString)")
-            } catch {
-              logger.error("Error converting template \(templateKeyString): \(error)")
-            }
-          }
+          importPage(from: notebook, key: key, properties: properties, into: backgroundContext)
         }
         do {
           try backgroundContext.save()
@@ -71,6 +33,82 @@ public enum CoreDataImporter {
           logger.error("Unable to save core data changes: \(error)")
         }
       }
+    }
+  }
+}
+
+// MARK: - Private
+
+private extension CoreDataImporter {
+  static func importAllPages(
+    from notebook: NoteArchiveDocument,
+    into backgroundContext: NSManagedObjectContext
+  ) {
+    for (key, properties) in notebook.pageProperties {
+      importPage(from: notebook, key: key, properties: properties, into: backgroundContext)
+    }
+  }
+
+  static func importPage(
+    from notebook: NoteArchiveDocument,
+    key: String,
+    properties: PageProperties,
+    into backgroundContext: NSManagedObjectContext
+  ) {
+    let uuid = UUID(uuidString: key)!
+    let page = CDPage.getOrCreate(uuid: uuid, context: backgroundContext)
+    page.timestamp = properties.timestamp
+    page.title = properties.title
+    page.hashtags = NSSet(array: properties.hashtags
+      .map { CDHashtag.getOrCreate(name: $0, context: backgroundContext) }
+    )
+    importPageContents(from: notebook, key: key, into: page)
+    // Delete all existing templates
+    backgroundContext.deleteAllObjects(in: page.challengeTemplates)
+    // Import templates
+    page.challengeTemplates = NSSet(array: properties.cardTemplates
+      .compactMap {
+        try? convertTemplate(identifierString: $0, notebook: notebook, context: backgroundContext)
+      }
+    )
+    logger.info("page: \(page)")
+  }
+
+  static func convertTemplate(
+    identifierString: String,
+    notebook: NoteArchiveDocument,
+    context: NSManagedObjectContext
+  ) throws -> CDChallengeTemplate? {
+    guard
+      let templateKey = ChallengeTemplateArchiveKey(identifierString),
+      let template = notebook.challengeTemplate(for: identifierString)
+    else {
+      return nil
+    }
+    let templateObject = CDChallengeTemplate(context: context)
+    templateObject.serialized = try YAMLEncoder().encode(template)
+    templateObject.type = templateKey.type
+    // Import challenges
+    let challenges = template.challenges.map { challenge -> CDChallenge in
+      let challengeObject = CDChallenge(context: context)
+      challengeObject.key = String(describing: challenge.challengeIdentifier.index)
+      return challengeObject
+    }
+    templateObject.challenges = Set(challenges) as NSSet
+    return templateObject
+  }
+
+  static func importPageContents(
+    from notebook: NoteArchiveDocument,
+    key: String,
+    into page: CDPage
+  ) {
+    guard let context = page.managedObjectContext else { return }
+    if let contents = try? notebook.currentTextContents(for: key) {
+      page.contents.flatMap { context.delete($0) }
+      let contentsObject = CDPageContents(context: context)
+      contentsObject.contents = contents
+      page.contents = contentsObject
     }
   }
 }
