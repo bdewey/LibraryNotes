ad2dca96bb7d2250995992738aa9f991eb799831
Brian Dewey <bdewey@gmail.com>
2021-08-17 23:15:34 -0700

    Minor test case refactor

--- GrailDiaryTests/NoteSqliteStorageTests.swift
+++ GrailDiaryTests/NoteSqliteStorageTests.swift
@@ -2,88 +2,86 @@
 
 import Combine
 @testable import GrailDiary
+import KeyValueCRDT
 import XCTest
 
-final class NoteSqliteStorageTests: NoteSqliteStorageTestBase {
-  func testCanOpenNonexistantFile() async throws {
-    try await withEmptyDatabase { _ in
-      // NOTHING
+final class NoteSqliteStorageTests: XCTestCase {
+  private var database: NoteDatabase!
+
+  override func setUp() async throws {
+    let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+    let database = try NoteDatabase(fileURL: fileURL, author: Author(id: UUID(), name: "test"))
+    let success = await database.open()
+    if !success {
+      throw TestError.couldNotOpenDatabase
     }
+    self.database = database
+  }
+
+  override func tearDown() async throws {
+    _ = await database.close()
+    try FileManager.default.removeItem(at: database.fileURL)
   }
 
   func testRoundTripSimpleNoteContents() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.simpleTest)
-      XCTAssertTrue(database.hasUnsavedChanges)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(Note.simpleTest, roundTripNote)
-    }
+    let identifier = try database.createNote(Note.simpleTest)
+    XCTAssertTrue(database.hasUnsavedChanges)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(Note.simpleTest, roundTripNote)
   }
 
   func testUpdateNonExistantNoteCreatesNote() async throws {
-    try await withEmptyDatabase { database in
-      var note = Note.simpleTest
-      let identifier = UUID().uuidString
-      try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in
-        note
-      })
-      note.text = "Version 2.0 text"
-      try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in note })
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(note, roundTripNote)
-    }
+    var note = Note.simpleTest
+    let identifier = UUID().uuidString
+    try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in
+      note
+    })
+    note.text = "Version 2.0 text"
+    try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in note })
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(note, roundTripNote)
   }
 
   func testUpdateSimpleNote() async throws {
-    try await withEmptyDatabase { database in
-      var note = Note.simpleTest
-      let identifier = try database.createNote(note)
-      note.text = "Version 2.0 text"
-      try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in note })
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(note, roundTripNote)
-    }
+    var note = Note.simpleTest
+    let identifier = try database.createNote(note)
+    note.text = "Version 2.0 text"
+    try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in note })
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(note, roundTripNote)
   }
 
   func testRoundTripHashtagNoteContents() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.withHashtags)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(Note.withHashtags, roundTripNote)
-    }
+    let identifier = try database.createNote(Note.withHashtags)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(Note.withHashtags, roundTripNote)
   }
 
   func testRoundTripReferenceWebPage() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(.withReferenceWebPage)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(Note.withReferenceWebPage, roundTripNote)
-    }
+    let identifier = try database.createNote(.withReferenceWebPage)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(Note.withReferenceWebPage, roundTripNote)
   }
 
   func testUpdateHashtags() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.withHashtags)
-      try database.updateNote(noteIdentifier: identifier, updateBlock: { oldNote -> Note in
-        var note = oldNote
-        note.hashtags = ["#updated"]
-        return note
-      })
-      XCTAssertTrue(database.hasUnsavedChanges)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      var expectedNote = Note.withHashtags
-      expectedNote.hashtags = ["#updated"]
-      XCTAssertEqual(expectedNote, roundTripNote)
-    }
+    let identifier = try database.createNote(Note.withHashtags)
+    try database.updateNote(noteIdentifier: identifier, updateBlock: { oldNote -> Note in
+      var note = oldNote
+      note.hashtags = ["#updated"]
+      return note
+    })
+    XCTAssertTrue(database.hasUnsavedChanges)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    var expectedNote = Note.withHashtags
+    expectedNote.hashtags = ["#updated"]
+    XCTAssertEqual(expectedNote, roundTripNote)
   }
 
   func testUpdateNoteWithChallenges() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.withChallenges)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(Note.withChallenges, roundTripNote)
-      XCTAssertEqual(roundTripNote.promptCollections.count, 3)
-    }
+    let identifier = try database.createNote(Note.withChallenges)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(Note.withChallenges, roundTripNote)
+    XCTAssertEqual(roundTripNote.promptCollections.count, 3)
   }
 
   func testPartialQuoteDoesntFail() async throws {
@@ -92,59 +90,45 @@ final class NoteSqliteStorageTests: NoteSqliteStorageTestBase {
     >
 
     """)
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(note)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(note, roundTripNote)
-      XCTAssertEqual(roundTripNote.promptCollections.count, 1)
-    }
+    let identifier = try database.createNote(note)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(note, roundTripNote)
+    XCTAssertEqual(roundTripNote.promptCollections.count, 1)
   }
 
   func testRemoveChallengesFromNote() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.withChallenges)
-      try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in Note.simpleTest })
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(Note.simpleTest, roundTripNote)
-    }
+    let identifier = try database.createNote(Note.withChallenges)
+    try database.updateNote(noteIdentifier: identifier, updateBlock: { _ in Note.simpleTest })
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(Note.simpleTest, roundTripNote)
   }
 
   func testNotesShowUpInAllMetadata() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.withHashtags)
-      XCTAssertEqual(1, try database.bookMetadata.count)
-      XCTAssertEqual(try database.bookMetadata[identifier]?.title, Note.withHashtags.title)
-    }
+    let identifier = try database.createNote(Note.withHashtags)
+    XCTAssertEqual(1, database.bookMetadata.count)
+    XCTAssertEqual(try database.bookMetadata[identifier]?.title, Note.withHashtags.title)
   }
 
   func testCreatingNoteSendsNotification() async throws {
-    try await withEmptyDatabase { database in
-      var didGetNotification = false
-      let cancellable = database.notesDidChange.sink { didGetNotification = true }
-      _ = try database.createNote(Note.simpleTest)
-      XCTAssertTrue(didGetNotification)
-      cancellable.cancel()
-    }
+    var didGetNotification = false
+    let cancellable = database.notesDidChange.sink { didGetNotification = true }
+    _ = try database.createNote(Note.simpleTest)
+    XCTAssertTrue(didGetNotification)
+    cancellable.cancel()
   }
 
   func testDeleteNote() async throws {
-    try await withEmptyDatabase { database in
-      let identifier = try database.createNote(Note.withHashtags)
-      let roundTripNote = try database.note(noteIdentifier: identifier)
-      XCTAssertEqual(Note.withHashtags, roundTripNote)
-      XCTAssertEqual(1, try database.bookMetadata.count)
-      try database.deleteNote(noteIdentifier: identifier)
-      XCTAssertTrue(database.hasUnsavedChanges)
-      XCTAssertThrowsError(try database.note(noteIdentifier: identifier))
-      XCTAssertEqual(0, try database.bookMetadata.count)
-    }
+    let identifier = try database.createNote(Note.withHashtags)
+    let roundTripNote = try database.note(noteIdentifier: identifier)
+    XCTAssertEqual(Note.withHashtags, roundTripNote)
+    XCTAssertEqual(1, database.bookMetadata.count)
+    try database.deleteNote(noteIdentifier: identifier)
+    XCTAssertTrue(database.hasUnsavedChanges)
+    XCTAssertThrowsError(try database.note(noteIdentifier: identifier))
+    XCTAssertEqual(0, database.bookMetadata.count)
   }
 
   func testStudyLog() async throws {
-    let database = try await makeAndOpenEmptyKeyValueDatabase()
-    defer {
-      try? FileManager.default.removeItem(at: database.fileURL)
-    }
     _ = try database.createNote(Note.withChallenges)
     // New items aren't eligible for at 3-5 days.
     let future = Date().addingTimeInterval(5 * 24 * 60 * 60)
@@ -156,92 +140,81 @@ final class NoteSqliteStorageTests: NoteSqliteStorageTestBase {
     try database.updateStudySessionResults(studySession, on: Date(), buryRelatedPrompts: true)
     XCTAssertTrue(database.hasUnsavedChanges)
     XCTAssertEqual(database.studyLog.count, studySession.count)
-    _ = try await database.close()
   }
 
   func testChallengeStabilityAcrossUnrelatedEdits() async throws {
-    try await withEmptyDatabase { database in
-      let originalText = """
-      # Shakespeare quotes
+    let originalText = """
+    # Shakespeare quotes
 
-      > To be, or not to be, that is the question. (Hamlet)
+    > To be, or not to be, that is the question. (Hamlet)
 
-      """
-      let modifiedText = originalText.appending("> Out, out, damn spot! (Macbeth)\n")
-      let noteIdentifier = try database.createNote(Note(markdown: originalText))
-      let originalNote = try database.note(noteIdentifier: noteIdentifier)
-      let originalPromptKey = originalNote.promptCollections.first!.key
-      try database.updateNote(noteIdentifier: noteIdentifier, updateBlock: { note -> Note in
-        var note = note
-        XCTAssertEqual(note.promptCollections.count, 1)
-        note.updateMarkdown(modifiedText)
-        XCTAssertEqual(note.promptCollections.count, 2)
-        return note
-      })
-      let modifiedNote = try database.note(noteIdentifier: noteIdentifier)
-      XCTAssertEqual(originalNote.promptCollections[originalPromptKey]!.rawValue, modifiedNote.promptCollections[originalPromptKey]!.rawValue)
-      XCTAssertEqual(2, modifiedNote.promptCollections.count)
-    }
+    """
+    let modifiedText = originalText.appending("> Out, out, damn spot! (Macbeth)\n")
+    let noteIdentifier = try database.createNote(Note(markdown: originalText))
+    let originalNote = try database.note(noteIdentifier: noteIdentifier)
+    let originalPromptKey = originalNote.promptCollections.first!.key
+    try database.updateNote(noteIdentifier: noteIdentifier, updateBlock: { note -> Note in
+      var note = note
+      XCTAssertEqual(note.promptCollections.count, 1)
+      note.updateMarkdown(modifiedText)
+      XCTAssertEqual(note.promptCollections.count, 2)
+      return note
+    })
+    let modifiedNote = try database.note(noteIdentifier: noteIdentifier)
+    XCTAssertEqual(originalNote.promptCollections[originalPromptKey]!.rawValue, modifiedNote.promptCollections[originalPromptKey]!.rawValue)
+    XCTAssertEqual(2, modifiedNote.promptCollections.count)
   }
 
   func testChallengeStabilityWithTemplateEdits() async throws {
-    try await withEmptyDatabase { database in
-      let originalText = """
-      # Shakespeare quotes
+    let originalText = """
+    # Shakespeare quotes
 
-      > To be, or not to be, that is the question.
-      """
-      let modifiedText = originalText.appending(" (Hamlet)\n")
-      let noteIdentifier = try database.createNote(Note(markdown: originalText))
-      let originalNote = try database.note(noteIdentifier: noteIdentifier)
-      try database.updateNote(noteIdentifier: noteIdentifier, updateBlock: { note -> Note in
-        var note = note
-        XCTAssertEqual(note.promptCollections.count, 1)
-        note.updateMarkdown(modifiedText)
-        XCTAssertEqual(note.promptCollections.count, 1)
-        return note
-      })
-      let modifiedNote = try database.note(noteIdentifier: noteIdentifier)
-      XCTAssertEqual(originalNote.promptCollections.keys, modifiedNote.promptCollections.keys)
-      XCTAssertEqual(1, modifiedNote.promptCollections.count)
-      XCTAssertEqual(modifiedNote.promptCollections.first!.value.rawValue, "> To be, or not to be, that is the question. (Hamlet)\n")
-    }
+    > To be, or not to be, that is the question.
+    """
+    let modifiedText = originalText.appending(" (Hamlet)\n")
+    let noteIdentifier = try database.createNote(Note(markdown: originalText))
+    let originalNote = try database.note(noteIdentifier: noteIdentifier)
+    try database.updateNote(noteIdentifier: noteIdentifier, updateBlock: { note -> Note in
+      var note = note
+      XCTAssertEqual(note.promptCollections.count, 1)
+      note.updateMarkdown(modifiedText)
+      XCTAssertEqual(note.promptCollections.count, 1)
+      return note
+    })
+    let modifiedNote = try database.note(noteIdentifier: noteIdentifier)
+    XCTAssertEqual(originalNote.promptCollections.keys, modifiedNote.promptCollections.keys)
+    XCTAssertEqual(1, modifiedNote.promptCollections.count)
+    XCTAssertEqual(modifiedNote.promptCollections.first!.value.rawValue, "> To be, or not to be, that is the question. (Hamlet)\n")
   }
 
   func testSubstantialEditGetsNewKey() async throws {
-    try await withEmptyDatabase { database in
-      let originalText = """
-      # Shakespeare quotes
+    let originalText = """
+    # Shakespeare quotes
 
-      > To be, or not to be, that is the question.
-      """
-      let modifiedText = """
-      # Shakespeare quotes
+    > To be, or not to be, that is the question.
+    """
+    let modifiedText = """
+    # Shakespeare quotes
 
-      > Out, out, damn spot!
-      """
-      let noteIdentifier = try database.createNote(Note(markdown: originalText))
-      let originalNote = try database.note(noteIdentifier: noteIdentifier)
-      try database.updateNote(noteIdentifier: noteIdentifier, updateBlock: { note -> Note in
-        var note = note
-        XCTAssertEqual(note.promptCollections.count, 1)
-        note.updateMarkdown(modifiedText)
-        XCTAssertEqual(note.promptCollections.count, 1)
-        return note
-      })
-      let modifiedNote = try database.note(noteIdentifier: noteIdentifier)
-      // We should have *different* keys because we changed the quote substantially, not just a little edit.
-      XCTAssertNotEqual(originalNote.promptCollections.keys, modifiedNote.promptCollections.keys)
-      XCTAssertEqual(1, modifiedNote.promptCollections.count)
-      XCTAssertEqual(modifiedNote.promptCollections.first!.value.rawValue, "> Out, out, damn spot!")
-    }
+    > Out, out, damn spot!
+    """
+    let noteIdentifier = try database.createNote(Note(markdown: originalText))
+    let originalNote = try database.note(noteIdentifier: noteIdentifier)
+    try database.updateNote(noteIdentifier: noteIdentifier, updateBlock: { note -> Note in
+      var note = note
+      XCTAssertEqual(note.promptCollections.count, 1)
+      note.updateMarkdown(modifiedText)
+      XCTAssertEqual(note.promptCollections.count, 1)
+      return note
+    })
+    let modifiedNote = try database.note(noteIdentifier: noteIdentifier)
+    // We should have *different* keys because we changed the quote substantially, not just a little edit.
+    XCTAssertNotEqual(originalNote.promptCollections.keys, modifiedNote.promptCollections.keys)
+    XCTAssertEqual(1, modifiedNote.promptCollections.count)
+    XCTAssertEqual(modifiedNote.promptCollections.first!.value.rawValue, "> Out, out, damn spot!")
   }
 
   func testBuryRelatedChallenges() async throws {
-    let database = try await makeAndOpenEmptyKeyValueDatabase()
-    defer {
-      try? FileManager.default.removeItem(at: database.fileURL)
-    }
     _ = try database.createNote(Note.multipleClozes)
     // New items aren't eligible for at 3-5 days.
     let future = Date().addingTimeInterval(5 * 24 * 60 * 60)
@@ -257,6 +230,5 @@ final class NoteSqliteStorageTests: NoteSqliteStorageTestBase {
     XCTAssertEqual(studySession.count, 0)
     studySession = try await database.studySession(filter: nil, date: future.addingTimeInterval(24 * .hour))
     XCTAssertEqual(studySession.count, 1)
-    _ = await database.close()
   }
 }
