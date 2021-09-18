7f616e0208a79f37fb6db461fbb7675d28e20af1
Brian Dewey <bdewey@gmail.com>
2021-09-18 10:38:35 -0700

    Rename all the things

--- LibraryNotesTests/Note+TestContent.swift
+++ LibraryNotesTests/Note+TestContent.swift
@@ -1,7 +1,7 @@
 // Copyright (c) 2018-2021  Brian Dewey. Covered by the Apache 2.0 license.
 
 import Foundation
-import GrailDiary
+import LibraryNotes
 
 extension Note {
   static let simpleTest = Note(
--- LibraryNotesTests/QuoteTemplateTests.swift
+++ LibraryNotesTests/QuoteTemplateTests.swift
@@ -1,6 +1,6 @@
 // Copyright (c) 2018-2021  Brian Dewey. Covered by the Apache 2.0 license.
 
-import GrailDiary
+import LibraryNotes
 import TextMarkupKit
 import XCTest
 
