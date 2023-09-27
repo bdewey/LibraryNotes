a05f0e9ed191514a992cd088e6760fcd4be31d5e
Brian Dewey <bdewey@gmail.com>
2023-09-27 08:35:51 -0700

    Xcode 15 settings

--- LibraryNotes/SemanticColor.swift
+++ LibraryNotes/SemanticColor.swift
@@ -7,19 +7,7 @@ private let useSystemColors = false
 extension UIColor {
   static let grailTint = UIColor.systemOrange
 
-  static let grailBackground = useSystemColors
-    ? UIColor.systemBackground
-    : UIColor(named: "grailBackground")!
-
-  static let grailSecondaryBackground = useSystemColors
-    ? UIColor.secondarySystemBackground
-    : UIColor(named: "grailSecondaryBackground")!
-
   static let grailGroupedBackground = useSystemColors
     ? UIColor.systemGroupedBackground
     : UIColor(named: "grailSecondaryBackground")!
-
-  static let grailSecondaryGroupedBackground = useSystemColors
-    ? UIColor.secondarySystemGroupedBackground
-    : UIColor(named: "grailBackground")!
 }
